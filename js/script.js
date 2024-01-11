968c0732a9bbee8d622bc40b15fae759c898da75
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-01-11 19:11:25 +0100

    milestone_3

--- js/script.js
+++ js/script.js
@@ -5,9 +5,6 @@ const buttonPlay = document.querySelector("button");
 const grid = document.getElementById("grid");
 const scoreSpan = document.getElementById("your-score");
 
-// dichiaro variabili
-let score = 0;
-
 // * funzioni
 // funzione per creare celle
 const createCells = (cellsTotal, score) => {
@@ -22,13 +19,26 @@ newCell.addEventListener("click", function() {
     if (newCell.classList.contains("clicked")) return;
     newCell.classList.add("clicked");
     console.log(newCell.innerText);
+
+    // capire se contiene bombe
+    const hasHitBomb = bombs.includes(parseInt(newCell.innerText));
+    if (hasHitBomb) {
+        console.log("Bomba pestata! Hai perso :(");
+        newCell.classList.add("bomb");
+    }
+
+    // aumentare punteggio
     scoreSpan.innerText = ++score;
     });
 }};
 
-// funzione per generare bombe
-const generateBombs = (totalCells, bombNumber) => {
+
+// dichiaro array bombe
 const bombs = [];
+
+// funzione per generare bombe
+const createBombs = (totalCells, bombNumber) => {
+
 while (bombs.length < bombNumber){
     const newBomb = Math.floor(Math.random() * totalCells) + 1
     if (!bombs.includes(newBomb)) {
@@ -36,6 +46,7 @@ while (bombs.length < bombNumber){
     }
 };
 console.log(bombs);
+return bombs;
 }
 
 // * svolgimento
@@ -53,30 +64,44 @@ formPlay.addEventListener("submit", function(event) {
     //rimuovo vecchie classi
     grid.classList.remove("hard", "medium", "easy");
 
+    // dichiaro variabile punteggio e la resetto
+    let score = 0;
+    scoreSpan.innerText = score;
+
     // individuare quantità celle per riga, colonna e totali
     // dichiaro variabili
     let cellsCol;
     let cellsRow;
+    let bombNumber;
 
     switch (selectDifficulty.value) {
         case "easy":
             cellsCol = 10;
             cellsRow = 10;
+            bombNumber = 15;
             break
         case "medium":
             cellsCol = 9;
             cellsRow = 9;
+            bombNumber = 17;
             break
         case "hard":
             cellsCol = 7;
             cellsRow = 7;
+            bombNumber = 20;
     }
 
     // calcolo le celle totali che compongono la griglia
     const cellsTotal = cellsCol * cellsRow;
 
+    // punteggio massimo
+    const maxScore = cellsTotal - bombNumber;
+
     // inserisco classe alla griglia per formattare le celle
     grid.classList.add(selectDifficulty.value);
 
+    createBombs(cellsTotal, bombNumber);
+
     createCells(cellsTotal, score);
+
 });
